376ef71b0e3db1567b2b5c134111026f4b10d504
Pedro Soares <pmbs.123@gmail.com>
2023-09-30 14:47:07 +0200

    feat: create a configuration file

---
+++ include/configuration.hpp
@@ -0,0 +1,45 @@
+#ifndef CONFIGURATION_HPP
+#define CONFIGURATION_HPP
+
+// -----------------------------------------------------------------------------
+// ---------------------------| Motor GPIO Pins |-------------------------------
+// -----------------------------------------------------------------------------
+
+// GPIO pin configuration for the right motor.
+#define GPIO_MOTOR_RIGHT_EN 10  // Must be a PWM pin
+#define GPIO_MOTOR_RIGHT_IN1 6
+#define GPIO_MOTOR_RIGHT_IN2 7
+#define GPIO_MOTOR_RIGHT_ENCODER_A 3  // Must be a hardware interrupt pin
+#define GPIO_MOTOR_RIGHT_ENCODER_B 5
+
+// GPIO pin configuration for the left motor.
+#define GPIO_MOTOR_LEFT_EN 11  // Must be a PWM pin
+#define GPIO_MOTOR_LEFT_IN1 8
+#define GPIO_MOTOR_LEFT_IN2 9
+#define GPIO_MOTOR_LEFT_ENCODER_A 2  // Must be a hardware interrupt pin
+#define GPIO_MOTOR_LEFT_ENCODER_B 4
+
+// -----------------------------------------------------------------------------
+// ----------------------------| Motor Configuration |--------------------------
+// -----------------------------------------------------------------------------
+
+// Motor and encoder configuration settings.
+#define WHEEL_RADIUS 0.0339  // Wheel radius in meters
+#define ENCODER_TICKS_PER_REVOLUTION 490
+
+// PID gains
+#define MOTOR_DRIVER_PID_KP 500
+#define MOTOR_DRIVER_PID_KI 130.0
+#define MOTOR_DRIVER_PID_KD 50.0
+
+#define MOTOR_RUN_FREQUENCY 20  // Motor run frequency (in Hz)
+
+// -----------------------------------------------------------------------------
+// ----------------------------| Serial Configuration |-------------------------
+// -----------------------------------------------------------------------------
+
+// If you change this and want to use platformIO monitor, you must also change
+// the baud rate in platformio.ini
+#define SERIAL_BAUD_RATE 9600
+
+#endif  // !CONFIGURATION_HPP
