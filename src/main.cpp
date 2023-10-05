74602c49e7ef2471ac91f2fcfcb338adcdb57613
Pedro Soares <pmbs.123@gmail.com>
2023-10-05 19:05:08 +0200

    feat: make use of the motor controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,41 @@
 #include <Arduino.h>
 
 #include "configuration.hpp"
+#include "motor_controller.hpp"
 #include "motor_driver.hpp"
 #include "timer.hpp"
 
-Encoder encoder(GPIO_MOTOR_LEFT_ENCODER_A, GPIO_MOTOR_LEFT_ENCODER_B, true);
-MotorDriver motor(GPIO_MOTOR_LEFT_EN,
-                  GPIO_MOTOR_LEFT_IN1,
-                  GPIO_MOTOR_LEFT_IN2,
-                  &encoder,
-                  WHEEL_RADIUS,
-                  ENCODER_TICKS_PER_REVOLUTION,
-                  true);
-Timer timer(50);
+Encoder left_motor_encoder(GPIO_MOTOR_LEFT_ENCODER_A, GPIO_MOTOR_LEFT_ENCODER_B, true);
+Encoder right_motor_encoder(GPIO_MOTOR_RIGHT_ENCODER_A, GPIO_MOTOR_RIGHT_ENCODER_B);
+MotorDriver left_motor(GPIO_MOTOR_LEFT_EN,
+                       GPIO_MOTOR_LEFT_IN1,
+                       GPIO_MOTOR_LEFT_IN2,
+                       &left_motor_encoder,
+                       WHEEL_RADIUS,
+                       ENCODER_TICKS_PER_REVOLUTION,
+                       true);
+MotorDriver right_motor(GPIO_MOTOR_RIGHT_EN,
+                        GPIO_MOTOR_RIGHT_IN1,
+                        GPIO_MOTOR_RIGHT_IN2,
+                        &right_motor_encoder,
+                        WHEEL_RADIUS,
+                        ENCODER_TICKS_PER_REVOLUTION);
 
-void encoder_isr(void) { encoder.tick_isr(); }
+MotorController motor_controller(&left_motor, &right_motor, DIST_BETWEEN_WHEELS);
+
+void left_motor_encoder_ISR(void) { left_motor_encoder.tick_isr(); };
+void right_motor_encoder_ISR(void) { right_motor_encoder.tick_isr(); };
+
+void setup_interrupts(void) {
+    attachInterrupt(digitalPinToInterrupt(GPIO_MOTOR_LEFT_ENCODER_A),
+                    left_motor_encoder_ISR,
+                    RISING);
+    attachInterrupt(digitalPinToInterrupt(GPIO_MOTOR_RIGHT_ENCODER_A),
+                    right_motor_encoder_ISR,
+                    RISING);
+}
+
+CmdVel cmd_vel = {0.3, 1.0};
 
 /**
  * The setup function is called once at startup of the sketch
@@ -22,24 +43,14 @@ void encoder_isr(void) { encoder.tick_isr(); }
 void setup(void) {
     Serial.begin(9600);
     Serial.println("Start");
-    attachInterrupt(
-        digitalPinToInterrupt(GPIO_MOTOR_LEFT_ENCODER_A), encoder_isr, RISING);
-    motor.set_velocity(0.5);
+    setup_interrupts();
+    motor_controller.set_cmd_vel(cmd_vel);
 }
 
 /**
  * The loop function is called in an endless loop
  */
 void loop(void) {
-    if (timer.has_elapsed()) {
-        motor.run();
-        MotorData data;
-        motor.get_motor_data(data);
-        Serial.print("Setpoint:");
-        Serial.print(0.5);
-        Serial.print(",");
-        Serial.print("velocity:");
-        Serial.println(data.velocity);
-        motor.print_status();
-    }
+    motor_controller.run();
+    motor_controller.print_pose();
 }
